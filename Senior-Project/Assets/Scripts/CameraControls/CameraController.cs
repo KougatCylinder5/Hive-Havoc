d7cf90fda89d8d72bf78c51cb87d21d01e931d8c
Kyle Hageman <kjh040699@gmail.com>
2023-09-08 14:57:01 -0400

    started camera controls

---
+++ Senior-Project/Assets/Scripts/CameraControls/CameraController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float movementTime;
+    public Vector3 newPosition;
+
+    public Vector3 zoomAmount;
+    public Vector3 newZoom;
+    public Transform cameraTransform;
+
+    public Vector3 dragStartPosition;
+    public Vector3 dragCurrentPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+        newZoom = cameraTransform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+        HandleMouseInput();
+    }
+
+    void HandleMovementInput()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += transform.forward * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += transform.forward * -movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += transform.right * -movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += transform.right * movementSpeed;
+        }
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+    }
+
+    void HandleMouseInput()
+    {
+        if(Input.mouseScrollDelta.y != 0)
+        {
+            newZoom += Input.mouseScrollDelta.y * zoomAmount;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            Plane plane = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float entry;
+            if(plane.Raycast(ray, out entry))
+            {
+                dragStartPosition = ray.GetPoint(entry);
+            }
+        }
+
+
+        cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, newZoom, Time.deltaTime * movementTime);
+    }    
+}
