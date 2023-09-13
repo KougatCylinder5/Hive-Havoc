d6c200a3f46452636495f836d5eb4c33b20cf074
Blake School <blakegrime06@gmail.com>
2023-09-13 14:58:04 -0400

    Screen resolution Should work

--- Senior-Project/Assets/Scripts/SettingsMenu.cs
+++ Senior-Project/Assets/Scripts/SettingsMenu.cs
@@ -1,11 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+
+    public AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
+    public Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++) 
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
     public void SetVolume (float volume)
     {
-        
+        audioMixer.SetFloat("volume", volume/100 * 80 - 80);
+    }
+    public void SetQuality( int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
     }
 }
