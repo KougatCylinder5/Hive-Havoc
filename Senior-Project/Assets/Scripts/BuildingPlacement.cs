15a2360b953860b59356deefe35f7a8a03ce1bfb
Foofoo99-fccc <112097785+Foofoo99@users.noreply.github.com>
2023-10-13 14:47:46 -0400

    building placement works

--- Senior-Project/Assets/Scripts/BuildingPlacement.cs
+++ Senior-Project/Assets/Scripts/BuildingPlacement.cs
@@ -1,6 +1,8 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -9,41 +11,49 @@ public class BuildingPlacement : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> buildingPrefabs;
-    public bool inPlaceMode = false;
-    public GameObject currentBuilding;
-    public GameObject ghostBuilding;
+    private bool inPlaceMode = false;
+    private GameObject currentBuilding;
+    private GameObject ghostBuilding;
     private bool _made;
+    private List<KeyCode> keycodes = new();
+    private int _pressed;
 
     private enum Buildings
     {
         Base,
         BaseGhost,
         Tent,
-        Wood_House,
-        Stone_House
+        TentGhost,
+        Tower,
+        TowerGhost
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        keycodes.Add(KeyCode.Z); 
+        keycodes.Add(KeyCode.X);
+        keycodes.Add(KeyCode.C);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 position = Mouse.MouseToWorldPoint(LayerMask.GetMask("Terrain", "Water"));
-
-        if (Input.GetKeyDown(KeyCode.B))
+        foreach(KeyCode key in keycodes)
         {
-            inPlaceMode = true;
+            if(Input.GetKeyDown(key))
+            {
+                inPlaceMode = true;
+                _pressed = keycodes.IndexOf(key);
+            }
         }
         if(inPlaceMode)
         {
             if(!_made)
             {
-                ghostBuilding = Instantiate(buildingPrefabs[(int)Buildings.BaseGhost], new Vector3(position.x, 1, position.y), Quaternion.identity);
-                currentBuilding = buildingPrefabs[(int)Buildings.Base];
+                ghostBuilding = Instantiate(buildingPrefabs[_pressed * 2 + 1], new Vector3(position.x, 0.5f, position.y), Quaternion.identity);
+                currentBuilding = buildingPrefabs[_pressed * 2];
                 _made = true;
             }
             showBuilding(ghostBuilding, position);
@@ -59,6 +69,25 @@ public class BuildingPlacement : MonoBehaviour
 
     public void showBuilding(GameObject building, Vector2 position)
     {
-        building.transform.position = new Vector3((int)position.x + 0.5f, 1, (int)position.y + 0.5f);
+        building.transform.position = new Vector3((int)position.x + sizeCheck(building, 1), 0.5f, (int)position.y + sizeCheck(building, 2));
+    }
+
+    public float sizeCheck(GameObject building, int axis)
+    {
+        if(axis == 1)
+        {
+            if (building.transform.localScale.x % 2 == 0)
+            {
+                return 0.5f;
+            }
+        }
+        if(axis == 2)
+        {
+            if (building.transform.localScale.z % 2 == 0)
+            {
+                return 0.5f;
+            }
+        }
+        return 0;
     }
 }
