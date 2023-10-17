235fc4582998f90976ecc8d684b89b16c9f2e97b
FCCC- Cory Mavis <58046422+KougatCylinder5@users.noreply.github.com>
2023-10-17 12:28:16 -0400

    Added more units for brute force testing

--- Senior-Project/Assets/Scripts/SpawnBugs.cs
+++ Senior-Project/Assets/Scripts/SpawnBugs.cs
@@ -10,7 +10,7 @@ public class SpawnBugs : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnBug", 0, 5);
+        InvokeRepeating(nameof(SpawnBug), 0, 1);
     }
 
     // Update is called once per frame
