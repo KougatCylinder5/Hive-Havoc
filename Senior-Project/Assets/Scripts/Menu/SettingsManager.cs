ebe590eebacf4d7625783360bfea3930639a74b5
Blake School <blakegrime06@gmail.com>
2023-11-03 14:51:22 -0400

    added keybinds menu

--- Senior-Project/Assets/Scripts/Menu/SettingsManager.cs
+++ Senior-Project/Assets/Scripts/Menu/SettingsManager.cs
@@ -5,14 +5,25 @@ using UnityEngine.UI;
 using TMPro;
 using System.Runtime.CompilerServices;
 using UnityEditor;
-using UnityEditor.Search;
 using UnityEngine.UIElements;
+using System;
+using static System.Net.Mime.MediaTypeNames;
 
 public class SettingsManager : MonoBehaviour
 {
     public TMP_Dropdown resolutionDropdown;
 
     Resolution[] resolutions;
+
+    public static KeyCode key1 = KeyCode.Alpha1;
+    public static KeyCode key2 = KeyCode.Alpha2;
+    public static KeyCode key3 = KeyCode.Alpha3;
+    public static KeyCode key4 = KeyCode.Alpha4;
+    public static KeyCode key5 = KeyCode.Alpha5;
+    public static KeyCode key6 = KeyCode.Alpha6;
+    public static KeyCode key7 = KeyCode.Alpha7;
+    public static KeyCode key8 = KeyCode.Alpha8;
+    public static KeyCode key9 = KeyCode.Alpha9;
 
     private void Start()
     {
@@ -28,7 +39,7 @@ public class SettingsManager : MonoBehaviour
         //gets all of the possible screen resolutions and formats them
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height + " " + float.Parse(resolutions[i].refreshRateRatio.ToString()) + "hz";
+            string option = resolutions[i].width + " x " + resolutions[i].height + " " + decimal.Round(decimal.Parse(resolutions[i].refreshRateRatio.ToString()), 2) + "hz";
             options.Add(option);
 
             //sees if the screen size is the users screen size
@@ -42,6 +53,11 @@ public class SettingsManager : MonoBehaviour
         //sets the resolution to users defult screen size
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+    }
+
+    private void Update()
+    {
+        printKeys();
     }
 
     public void setResolution(int resolutionIndex)
@@ -58,5 +74,18 @@ public class SettingsManager : MonoBehaviour
     public void setFullScreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
+    }
+
+    public void printKeys()
+    {
+        //Looks at each key that could possibly be pressed
+        foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+        {
+            //if the key it is looking at is down, print it
+            if (Input.GetKeyDown(kcode))
+            {
+                Debug.Log(kcode);
+            }
+        }
     }
 }
