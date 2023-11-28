4d2ba18e63f5eaa6a6b3d3b77c7bb4356601b456
FCCC- Cory Mavis <58046422+KougatCylinder5@users.noreply.github.com>
2023-11-28 13:44:36 -0500

    Ghost Prefabs no longer have hitboxes and placeable objects can now be saved

--- Senior-Project/Assets/Scripts/Database/Saver.cs
+++ Senior-Project/Assets/Scripts/Database/Saver.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -17,71 +16,86 @@ public class Saver : MonoBehaviour
     {
         //ground = Instantiate(groundPrefab, new Vector3(0,0,0), groundPrefab.transform.rotation).GetComponent<Terrain>();
         ground = GameObject.Find("Ground").GetComponent<Terrain>();
-        
+
         ground.terrainData = Instantiate(groundData);
 
         DBAccess.fixItQuick = ground.terrainData.treeInstances;
 
-        if (DBAccess.isAReload()) {
-            DBAccess.clearReload();
-
-            DBAccess.startTransaction();
-
-            List<Placeable> naturalObjects = DBAccess.getPlaceables();
-
-            Vector3 worldSize = ground.terrainData.size;
-
-            ground.terrainData.treeInstances = new TreeInstance[0];
-
-            naturalObjects.ForEach(placeable => 
-            {
-                if (placeable.getTileItemID() != (int)PlaceableTypes.Tree)
-                    return;
-
-                PathingManager.SetWalkable(Mathf.RoundToInt(Mathf.Clamp(placeable.getXPos()*worldSize.x,0, worldSize.x)), Mathf.RoundToInt(Mathf.Clamp(placeable.getYPos()*worldSize.z, 0, worldSize.z)), false);
-                GameObject occuluder = Instantiate (treeOcculuderPrefab, new Vector3Int(Mathf.RoundToInt(Mathf.Clamp(placeable.getXPos() * worldSize.x, 0, worldSize.x)),0, Mathf.RoundToInt(Mathf.Clamp(placeable.getYPos() * worldSize.z, 0, worldSize.z))), Quaternion.identity, ground.gameObject.transform);
-                occuluder.tag = "Tree Hitbox";
-
-
-                for (int i = 0; i < 5; i++)
-                {
-                    Vector3 position = new Vector3(placeable.getXPos(),0, placeable.getYPos());
-
-                    Vector2 randomOffset = Random.insideUnitSphere;
-
-                    position += new Vector3(randomOffset.x, 0, randomOffset.y)/250;
-
-                    ground.AddTreeInstance(new TreeInstance
-                    {
-                        prototypeIndex = 0,
-                        position = position,
-                        rotation = Random.Range(0, 1),
-                        widthScale = 1,
-                        heightScale = 1
-                    });
-                }
-
-
-
-
-            });
-            ground.Flush();
-            DBAccess.commitTransaction();
-        } else {
+        if (!DBAccess.isAReload())
+        {
             saveScene();
         }
+        DBAccess.clearReload();
+        DBAccess.startTransaction();
+
+        List<Placeable> naturalObjects = DBAccess.getPlaceables();
+
+        Vector3 worldSize = ground.terrainData.size;
+
+        ground.terrainData.treeInstances = new TreeInstance[0];
+
+        naturalObjects.ForEach(placeable =>
+        {
+            if (placeable.getTileItemID() == (int)PlaceableTypes.Tree) {
+                PathingManager.SetWalkable(Mathf.RoundToInt(Mathf.Clamp(placeable.getXPos() * worldSize.x, 0, worldSize.x)), Mathf.RoundToInt(Mathf.Clamp(placeable.getYPos() * worldSize.z, 0, worldSize.z)), false);
+                GameObject occuluder = Instantiate(treeOcculuderPrefab, new Vector3Int(Mathf.RoundToInt(Mathf.Clamp(placeable.getXPos() * worldSize.x, 0, worldSize.x)), 0, Mathf.RoundToInt(Mathf.Clamp(placeable.getYPos() * worldSize.z, 0, worldSize.z))), Quaternion.identity, ground.gameObject.transform);
+                occuluder.tag = "Tree Hitbox";
+            }
+
+            int amount = 0;
+
+            switch (placeable.getTileItemID())
+            {
+                case 0:
+                    amount = 3;
+                    break;
+                case 1:
+                    amount = 1;
+                    break;
+            }
+
+
+
+
+
+            for (int i = 0; i < amount; i++)
+            {
+                Vector3 position = new Vector3(placeable.getXPos(), 0, placeable.getYPos());
+
+                Vector2 randomOffset = Random.onUnitSphere;
+
+                position += new Vector3(randomOffset.x, 0, randomOffset.y) / 200;
+
+                ground.AddTreeInstance(new TreeInstance
+                {
+                    prototypeIndex = placeable.getTileItemID(),
+                    position = position,
+                    rotation = Random.Range(0, 360),
+                    widthScale = 1,
+                    heightScale = 1
+                });
+            }
+
+
+
+
+        });
+        ground.Flush();
+        DBAccess.commitTransaction();
     }
 
-    public void saveScene() {
+    public void saveScene()
+    {
         DBAccess.startTransaction();
         DBAccess.clear();
 
-        GameObject.FindGameObjectsWithTag("Tree Hitbox").ToList().ForEach(gameObject => { DBAccess.addPlaceable(0, gameObject.transform.position.x / worldSize.x, gameObject.transform.position.z / worldSize.z, 1, 1); });
+        groundData.treeInstances.ToList().ForEach(tree => {DBAccess.addPlaceable(tree.prototypeIndex, tree.position.x, tree.position.z, 1, 1); });
 
         DBAccess.commitTransaction();
     }
 
-    public void quickFix() {
+    public void quickFix()
+    {
         ground.terrainData.treeInstances = DBAccess.fixItQuick;
         ground.Flush();
     }
