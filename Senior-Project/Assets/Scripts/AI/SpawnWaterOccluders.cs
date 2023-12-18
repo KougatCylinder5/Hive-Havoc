e435c3024a5cdcd035622a8d82c53689e22289d2
Foofoo99-fccc <112097785+Foofoo99@users.noreply.github.com>
2023-12-18 12:53:54 -0500

    keeping commits under file size limit

---
+++ Senior-Project/Assets/Scripts/AI/SpawnWaterOccluders.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PathingManager;
+
+public class SpawnWaterOccluders : MonoBehaviour
+{
+
+    private GameObject terrain;
+
+    public GameObject occluder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GameObject.Find("Ground");
+        Terrain groundComp = terrain.GetComponent<Terrain>();
+        int size = groundComp.terrainData.alphamapResolution;
+        print(size);
+        for (int i = 0; i < Mathf.Pow(size,2); i += Mathf.RoundToInt(size/groundComp.terrainData.size.x * 6f))
+        {
+            if (groundComp.terrainData.alphamapTextures[0].GetPixel(i / size, i % size).Equals(new(0, 0, 0, 1)))
+            {
+                ObstructedTiles[CalculateIndex(i / size * Mathf.RoundToInt(groundComp.terrainData.size.x) / size, i % size * Mathf.RoundToInt(groundComp.terrainData.size.z) / size)] = false;
+                Instantiate(occluder, new(i / size * Mathf.RoundToInt(groundComp.terrainData.size.x) / size, 0.1f, i % size * Mathf.RoundToInt(groundComp.terrainData.size.z) / size), Quaternion.identity, terrain.transform).layer = LayerMask.NameToLayer("Water");
+            }
+        }
+    }
+
+}
